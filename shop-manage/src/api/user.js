543016697e931371daa7a4473cf849f405319aaf
yueqianW <ames.yueqian@hotmail.com>
2022-02-27 13:59:13 +0800

    update vue

--- shop-manage/src/api/user.js
+++ shop-manage/src/api/user.js
@@ -1,8 +1,9 @@
 import request from '@/utils/request'
-
+// 权限管理: http://39.98.123.211:8170/swagger-ui.html
+// 商品管理：http://39.98.123.211:8216/swagger-ui.html
 export function login(data) {
   return request({
-    url: '/vue-admin-template/user/login',
+    url: '/admin/acl/index/login',
     method: 'post',
     data
   })
@@ -10,7 +11,7 @@ export function login(data) {
 
 export function getInfo(token) {
   return request({
-    url: '/vue-admin-template/user/info',
+    url: '/admin/acl/index/info',
     method: 'get',
     params: { token }
   })
@@ -18,7 +19,7 @@ export function getInfo(token) {
 
 export function logout() {
   return request({
-    url: '/vue-admin-template/user/logout',
+    url: '/admin/acl/index/logout',
     method: 'post'
   })
 }
