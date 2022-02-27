8296d9c326ee8d0106d24b801dce94363019f7f7
yueqianW <ames.yueqian@hotmail.com>
2022-02-27 09:12:05 +0800

    update vue

---
+++ shop/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <Header />
+    <router-view></router-view>
+    <Footer v-show="isFooterShow" />
+  </div>
+</template>
+
+<script>
+import Header from './components/Header';
+import Footer from './components/Footer';
+
+export default {
+  name: 'App',
+  components: {
+    Header,
+    Footer
+  },
+  computed: {
+    isFooterShow() {
+      return this.$route.meta.footer;
+    }
+  }
+};
+</script>
+
+<style>
+#app {
+  /* font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50; */
+}
+</style>
---
+++ shop/src/router/index.js
@@ -0,0 +1,45 @@
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import Home from '@/pages/Home';
+import Login from '@/pages/Login';
+import Register from '@/pages/Register';
+import Search from '@/pages/Search';
+
+Vue.use(VueRouter);
+
+export default new VueRouter({
+  routes: [
+    {
+      path: '/home',
+      component: Home,
+      meta: {
+        footer: true
+      }
+    },
+    {
+      path: '/search',
+      component: Search,
+      meta: {
+        footer: true
+      }
+    },
+    {
+      path: '/login',
+      component: Login,
+      meta: {
+        footer: false
+      }
+    },
+    {
+      path: '/register',
+      component: Register,
+      meta: {
+        footer: false
+      }
+    },
+    {
+      path: '*',
+      redirect: '/home'
+    }
+  ]
+});
